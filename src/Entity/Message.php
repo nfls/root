3d5ae5adfd7b339103e1e6865127b51bd44be5e2
Qingyang Hu <huqingyang2000@gmail.com>
2018-01-15 18:02:14 +0800

    Date phasing

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -203,5 +203,22 @@ class Message
         $this->priority = $priority;
     }
 
-    
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+
 }
