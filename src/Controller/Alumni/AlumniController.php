5e8366b67443ceba428350cdde15cdaf032f7743
胡清阳 <huqingyang2000@gmail.com>
2018-01-27 14:00:30 +0800

    Remove migratioins

--- src/Controller/Alumni/AlumniController.php
+++ src/Controller/Alumni/AlumniController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Alumni;
 
 use App\Controller\AbstractController;
+use App\Entity\Alumni;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,17 +11,34 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AlumniController extends AbstractController
 {
-    /**
-     * @Route("alumni/submit",methods="POST")
-     */
-    public function submitForm(Request $request){
-
-    }
 
     /**
      * @Route("alumni/form",methods="GET")
      */
     public function getForm(Request $request){
         return $this->response->response(json_decode(file_get_contents($this->get('kernel')->getRootDir()."/Controller/Alumni/Form.json")));
+    }
+
+    /**
+     * @Route("alumni/new", methods="POST")
+     */
+    public function newForm(Request $request){
+        $repo =  $this->getDoctrine()->getManager()->getRepository(Alumni::class);
+        if(count($repo->findBy(["user"=>$this->getUser(),"status"=>Alumni::STATUS_NOT_SUBMITTED]))>0){
+            return $this->response->response("alumni.already.new",403);
+        }
+        $alumni = new Alumni();
+        $alumni->setUser($this->getUser());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($alumni);
+        $em->flush();
+        return $this->response->response(null);
+    }
+
+    /**
+     * @Route("alumni/submit",methods="POST")
+     */
+    public function submitForm(Request $request){
+
     }
 }
--- src/Entity/Alumni.php
+++ src/Entity/Alumni.php
@@ -9,8 +9,7 @@ use Ramsey\Uuid\Doctrine\UuidType;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AlumniRepository")
  */
-class Alumni
-{
+class Alumni {
     /**
      * @var UuidType
      *
@@ -31,49 +30,55 @@ class Alumni
     /**
      * @var \DateTime
      *
-     * @ORM\Column(type="datetimetz")
+     * @ORM\Column(type="datetimetz", nullable=true)
      */
     private $submitTime;
 
     /**
      * @var boolean
      *
-     * @ORM\Column(type="smallint")
+     * @ORM\Column(type="smallint", nullable=true)
      */
     private $status;
+
+    const STATUS_NOT_SUBMITTED = 0;
+    const STATUS_SUBMITTED = 1;
+    const STATUS_REVIEWING = 2;
+    const STATUS_REJECTED = 3;
+    const STATUS_PASSED = 4;
 
     /**
      * @var string
      *
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $chineseName;
 
     /**
      * @var string
      *
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $englishName;
 
     /**
      * @var \DateTime
      *
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="date", nullable=true)
      */
     private $birthday;
 
     /**
      * @var integer
      *
-     * @ORM\Column(type="smallint")
+     * @ORM\Column(type="smallint", nullable=true)
      */
     private $gender;
 
     /**
      * @var integer
      *
-     * @ORM\Column(type="smallint")
+     * @ORM\Column(type="smallint", nullable=true)
      */
     private $juniorSchool;
 
@@ -94,7 +99,7 @@ class Alumni
      /**
       * @var integer
       *
-      * @ORM\Column(type="smallint")
+      * @ORM\Column(type="smallint", nullable=true)
       */
     private $seniorSchool;
 
@@ -143,14 +148,14 @@ class Alumni
     /**
      * @var string
      *
-     * @ORM\Column(type="string",length=100)
+     * @ORM\Column(type="string",length=100, nullable=true)
      */
     private $onlineContact;
 
     /**
      * @var string
      *
-     * @ORM\Column(type="json",length=256)
+     * @ORM\Column(type="json",length=256, nullable=true)
      */
     private $location;
 
@@ -165,4 +170,310 @@ class Alumni
     {
         $this->status = 0;
     }
+
+    /**
+     * @return bool
+     */
+    public function isStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param bool $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChineseName()
+    {
+        return $this->chineseName;
+    }
+
+    /**
+     * @param string $chineseName
+     */
+    public function setChineseName($chineseName)
+    {
+        $this->chineseName = $chineseName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnglishName()
+    {
+        return $this->englishName;
+    }
+
+    /**
+     * @param string $englishName
+     */
+    public function setEnglishName($englishName)
+    {
+        $this->englishName = $englishName;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @param \DateTime $birthday
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param int $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJuniorSchool()
+    {
+        return $this->juniorSchool;
+    }
+
+    /**
+     * @param int $juniorSchool
+     */
+    public function setJuniorSchool($juniorSchool)
+    {
+        $this->juniorSchool = $juniorSchool;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJuniorRegistration()
+    {
+        return $this->juniorRegistration;
+    }
+
+    /**
+     * @param int $juniorRegistration
+     */
+    public function setJuniorRegistration($juniorRegistration)
+    {
+        $this->juniorRegistration = $juniorRegistration;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJuniorClass()
+    {
+        return $this->juniorClass;
+    }
+
+    /**
+     * @param int $juniorClass
+     */
+    public function setJuniorClass($juniorClass)
+    {
+        $this->juniorClass = $juniorClass;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSeniorSchool()
+    {
+        return $this->seniorSchool;
+    }
+
+    /**
+     * @param int $seniorSchool
+     */
+    public function setSeniorSchool($seniorSchool)
+    {
+        $this->seniorSchool = $seniorSchool;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSeniorRegistration()
+    {
+        return $this->seniorRegistration;
+    }
+
+    /**
+     * @param int $seniorRegistration
+     */
+    public function setSeniorRegistration($seniorRegistration)
+    {
+        $this->seniorRegistration = $seniorRegistration;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSeniorClass()
+    {
+        return $this->seniorClass;
+    }
+
+    /**
+     * @param int $seniorClass
+     */
+    public function setSeniorClass($seniorClass)
+    {
+        $this->seniorClass = $seniorClass;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUniversity()
+    {
+        return $this->university;
+    }
+
+    /**
+     * @param string $university
+     */
+    public function setUniversity($university)
+    {
+        $this->university = $university;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMajor()
+    {
+        return $this->major;
+    }
+
+    /**
+     * @param string $major
+     */
+    public function setMajor($major)
+    {
+        $this->major = $major;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWorkInfo()
+    {
+        return $this->workInfo;
+    }
+
+    /**
+     * @param string $workInfo
+     */
+    public function setWorkInfo($workInfo)
+    {
+        $this->workInfo = $workInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPersonalInfo()
+    {
+        return $this->personalInfo;
+    }
+
+    /**
+     * @param string $personalInfo
+     */
+    public function setPersonalInfo($personalInfo)
+    {
+        $this->personalInfo = $personalInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOnlineContact()
+    {
+        return $this->onlineContact;
+    }
+
+    /**
+     * @param string $onlineContact
+     */
+    public function setOnlineContact($onlineContact)
+    {
+        $this->onlineContact = $onlineContact;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRemark()
+    {
+        return $this->remark;
+    }
+
+    /**
+     * @param string $remark
+     */
+    public function setRemark($remark)
+    {
+        $this->remark = $remark;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+
 }
