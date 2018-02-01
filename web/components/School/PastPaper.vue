07264636cd6ac71f3aa1e44d0621e29a29e53cb9
胡清阳 <huqingyang2000@gmail.com>
2018-02-01 13:14:27 +0800

    Finish past paper function

--- web/components/School/PastPaper.vue
+++ web/components/School/PastPaper.vue
@@ -2,12 +2,14 @@
     <div class="browser">
         <md-list>
             <md-list-item v-if="path.length > 0" @click="back">
-                <md-icon>move_to_inbox</md-icon>
-                <span class="md-list-item-text">Back</span>
+                <md-icon>folder_open</md-icon>
+                <span class="md-list-item-text">..</span>
             </md-list-item>
-            <md-list-item v-for="item in displayItems" :key="item.etaf" @click="enter(item.name)">
-                <md-icon>move_to_inbox</md-icon>
-                <span class="md-list-item-text">{{item.name}}</span>
+            <md-list-item v-for="item in displayItems" :key="item.etaf" @click="enter(item)">
+                <md-icon v-if="item.size == 0">folder</md-icon>
+                <md-icon v-else-if="item.name.endsWith('pdf')">picture_as_pdf</md-icon>
+                <md-inco v-else>attachment</md-inco>
+                <span class="md-list-item-text">{{item.displayName}}</span>
             </md-list-item>
         </md-list>
     </div>
@@ -19,14 +21,15 @@
         data: () => ({
             fileInfo: [],
             path: [],
-            displayItems: []
+            displayItems: [],
+            client: null
         }),
         mounted: function() {
             this.axios.get("/oauth/downloadSts").then((response) => {
                 var data = response.data["data"]
                 var OSS = require('ali-oss').Wrapper;
 
-                var client = new OSS({
+                this.client = new OSS({
                     region: 'oss-cn-shanghai',
                     accessKeyId: data["AccessKeyId"],
                     accessKeySecret: data["AccessKeySecret"],
@@ -34,18 +37,18 @@
                     bucket: 'nfls-papers',
                     secure: true
                 });
-                this.loadFiles(client,"")
+                this.loadFiles("")
             })
         }, methods: {
-            loadFiles(client,next){
+            loadFiles(next){
                 var self = this
-                var result = client.list({
+                var result = this.client.list({
                     "max-keys": 1000,
                     "marker": next
                 }).then(function (result) {
                     self.fileInfo = self.fileInfo.concat(result.objects)
                     if(result.objects.length == 1000 && false){
-                        self.loadFiles(client,result.nextMarker)
+                        self.loadFiles(result.nextMarker)
                     }else{
                         console.log(self.fileInfo)
                     }
@@ -54,28 +57,41 @@
                 });
             }, getCurrentPath(){
                 var reducer = (accumulator, currentValue) => accumulator + "/" + currentValue;
-                var uri = this.path.reduce(reducer,"")
+                var uri = this.path.reduce(reducer,"") + "/"
                 if(uri.startsWith("/"))
                     uri = uri.slice(1)
+
                 return uri
-            }, enter(name) {
-                this.path.push(name)
+            }, enter(item) {
+                if(item.size == 0){
+                    this.path.push(item.displayName)
+                } else {
+                    var url = this.client.signatureUrl(item.name, {expires: 120})
+                    this.downloadURI(url,item.displayName)
+                }
             }, updateItems(val){
                 var self = this
-                this.displayItems = val.filter(function(object){
+                this.displayItems = this.fileInfo.filter(function(object){
                     if(object.name.endsWith("/")){
                         return object.name.split("/").length - 1 == self.path.length + 1 && object.name.startsWith(self.getCurrentPath())
                     }else{
                         return object.name.split("/").length == self.path.length + 1 && object.name.startsWith(self.getCurrentPath())
                     }
-                }).map(function(object){
-                    object.name = object.name.replace(self.getCurrentPath(),"").replace("/","")
+                })
+                this.displayItems = this.displayItems.map(function(object){
+                    object.displayName = object.name.replace(self.getCurrentPath(),"").replace("/","")
                     return object
                 })
-                console.log(this.displayItems)
             }, back() {
                 this.path.pop()
-                console.log(this.path)
+            }, downloadURI(uri, name) {
+                var link = document.createElement("a");
+                link.download = name;
+                link.href = uri;
+                link.target = "_blank";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
             }
         }, watch: {
             fileInfo: function(val) {
