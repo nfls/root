81878520aaeb2e175284846345d85632ed0ce8fc
胡清阳 <huqingyang2000@gmail.com>
2018-02-14 00:10:32 +0800

    Add md

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,7 +60,9 @@ module.exports = {
           resolve('node_modules/markdown-palettes/src'), 
           resolve('node_modules/ali-oss/dist'), 
           resolve('node_modules/ali-oss/lib'),
-          resolve('node_modules/ali-oss/shims')
+          resolve('node_modules/ali-oss/shims'),
+          resolve('node_modules/vue-material/dist'),
+          resolve('node_modules/vue-material/src')
         ]
       }, 
       {
