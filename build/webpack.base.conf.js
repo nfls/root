731b57b7f8ddbcf0965fd0cc92ca8d732e58f81c
胡清阳 <huqingyang2000@gmail.com>
2018-02-13 20:58:15 +0800

    Remove all optimization

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,10 +56,6 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       }, 
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-      },
-      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
