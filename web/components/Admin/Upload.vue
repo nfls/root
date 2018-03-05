2bbafba0e85210a0fab49c3154357dd5905ef2a0
胡清阳 <huqingyang2000@gmail.com>
2018-03-06 01:18:09 +0800

    Feature: improve resources uploading

--- web/components/Admin/Upload.vue
+++ web/components/Admin/Upload.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="upload" align="left">
-        <span>Target server is hk2.nfls.io</span>
+        <span>目标服务器是 hk2.nfls.io</span><br/>
+        <span><strong>仅供图片、游戏类资源使用。目前没有进度条，上传时请耐心等待</strong></span><br/>
+        <span>注意：此处上传的文件访问时不需要鉴权。您的文件可以被任何拥有链接的人访问及下载。此上传空间有定期垃圾清理，未在网页上引用的将会在一定时间后被删除。</span>
         <form>
             <md-field>
                <label>文件</label>
                 <md-file @md-change="change"/>
             </md-field>
         </form>
-        <md-button @click="upload">提交</md-button><br/>
+        <md-button @click="upload" :disabled="loading">提交</md-button><br/>
         <span>{{url}}</span>
     </div>
 </template>
@@ -17,7 +19,8 @@
         name: "Upload",
         data: () => ({
             file: null,
-            url: ""
+            url: "",
+            loading:false
         }),
         mounted: function() {
             this.$emit("changeTitle", "资源上传")
@@ -31,8 +34,14 @@
                     return
                 let formData = new FormData();
                 formData.append("file",this.file[0],this.file[0].name)
+                this.loading = true
                 this.axios.post("/admin/upload",formData).then((response) => {
                     this.url = response.data["data"]
+                    this.file = null
+                    this.loading = false
+                }).catch((error) => {
+                    this.url = "上传失败"
+                    this.loading = false
                 })
             }
         }
