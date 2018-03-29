6c10280b23b727a0c778398181b32e30ba21b74c
胡清阳 <huqingyang2000@gmail.com>
2018-03-29 19:29:25 +0800

    UI: color change

--- web/components/Components/App.vue
+++ web/components/Components/App.vue
@@ -352,8 +352,8 @@
     @import "~vue-material/dist/theme/engine"; // Import the theme engine
 
     @include md-register-theme("default", (
-            primary: #263238, // The primary color of your application
-            accent: #ff5722// The accent or secondary color
+            primary: #303f9f, // The primary color of your application
+            accent: #ff5722 // The accent or secondary color
     ));
     @import "~vue-material/dist/theme/all"; // Apply the theme
 </style>
