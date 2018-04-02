fa067f77faa89349aa6d85b61b45e46deb5c6bd7
胡清阳 <huqingyang2000@gmail.com>
2018-04-02 15:29:31 +0800

    Feature: add version check

--- src/Controller/User/DeviceController.php
+++ src/Controller/User/DeviceController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\User;
 
 use App\Controller\AbstractController;
+use App\Entity\OAuth\Client;
 use App\Entity\Preference;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DeviceController extends AbstractController
 {
@@ -14,5 +16,15 @@ class DeviceController extends AbstractController
     public function getHeader()
     {
         return $this->response()->response($this->setting()->get(Preference::IOS_ANNOUNCEMENT));
+    }
+
+    /**
+     * @Route("device/version", methods="GET")
+     */
+    public function version(Request $request)
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository(Client::class);
+        $client = $repo->getClientById($request->query->get("client_id"));
+        return $this->response()->response($client->getVersion());
     }
 }
