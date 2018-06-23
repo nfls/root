c1039a41459d79273113db55cc2815232ec4f0a4
Qingyang Hu <huqingyang2000@gmail.com>
2018-06-23 21:59:47 +0800

    Feature: directory uses auto-generating form

--- web/components/Alumni/Directory.vue
+++ web/components/Alumni/Directory.vue
@@ -7,22 +7,27 @@
             </md-card-header>
 
             <md-card-content>
-                <md-field>
-                    <label>姓名</label>
-                    <md-input v-model="name"></md-input>
-                </md-field>
-                <md-field>
-                    <label>初中学校</label>
-                    <md-select v-model="juniorSchool" name="movies" id="movies" multiple>
-                        <md-option value="fight-club">Fight Club</md-option>
-                        <md-option value="godfather">Godfather</md-option>
-                        <md-option value="godfather-ii">Godfather II</md-option>
-                        <md-option value="godfather-iii">Godfather III</md-option>
-                        <md-option value="godfellas">Godfellas</md-option>
-                        <md-option value="pulp-fiction">Pulp Fiction</md-option>
-                        <md-option value="scarface">Scarface</md-option>
-                    </md-select>
-                </md-field>
+                <div v-for="item in formItems" :key="item.key">
+                    <md-field v-if="item.search === 'fuzzy' || item.search === 'precise'">
+                        <label>{{ $t('form-' + item.key) }}</label>
+                        <md-input :name="item.key" :id="item.key" :autocomplete="item.key"
+                                  v-model="form[item.key]"></md-input>
+                    </md-field>
+                    <md-field v-if="item.search === 'multi'">
+                    <label>{{ $t('form-' + item.key) }}</label>
+                        <md-select :name="item.key" :id="item.key" :autocomplete="item.key"
+                                   v-model="form[item.key]" multiple>
+                            <md-option value="fight-club">Fight Club</md-option>
+                            <md-option value="godfather">Godfather</md-option>
+                            <md-option value="godfather-ii">Godfather II</md-option>
+                            <md-option value="godfather-iii">Godfather III</md-option>
+                            <md-option value="godfellas">Godfellas</md-option>
+                            <md-option value="pulp-fiction">Pulp Fiction</md-option>
+                            <md-option value="scarface">Scarface</md-option>
+                        </md-select>
+                    </md-field>
+                </div>
+
             </md-card-content>
 
             <md-card-actions>
@@ -38,10 +43,23 @@
         name: "Directory",
         data: () => ({
             name: "",
-            juniorSchool: ""
+            juniorSchool: "",
+            formItems: [],
+            form: {}
         }),
         mounted: function () {
             this.$emit("changeTitle", this.$t('title-directory'))
+            this.load()
+        },
+        methods: {
+            load() {
+                this.axios.get("/alumni/form").then((response) => {
+                    this.formItems = response.data["data"].filter((object) => {
+                        return object.search
+                    })
+                    console.log(this.formItems)
+                })
+            }
         }
     }
 </script>
