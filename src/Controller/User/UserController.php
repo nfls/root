03fe9884c0f5065cd2ed919f6f6fad28f90376f3
Qingyang Hu <huqingyang2000@gmail.com>
2018-07-20 17:39:52 +0800

    Bug: fix null alumni

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -486,13 +486,18 @@ class UserController extends AbstractController
      * @Route("/user/page")
      */
     public function page(Request $request, TranslatorInterface $translator, CacheService $service) {
-        $this->denyAccessUnlessGranted(Permission::IS_AUTHENTICATED);
+        #$this->denyAccessUnlessGranted(Permission::IS_AUTHENTICATED);
         $id = $request->query->get("id");
         /** @var User $user */
         $user = $this->getDoctrine()->getManager()->getRepository(User::class)->find($id);
         if($service->antiSpiderUse($this->getUser(), $id) || $this->getUser()->isAdmin() || !$user->isAntiSpider()){
             /** @var Alumni $alumni */
             $alumni = $this->getDoctrine()->getManager()->getRepository(Alumni::class)->getLastSuccessfulAuth($user);
+            if(is_null($alumni))
+                return $this->response()->responseEntity(array(
+                    "alumni" => null,
+                    "info" => $user
+                ));
             $sender = $this->getDoctrine()->getManager()->getRepository(Alumni::class)->getLastSuccessfulAuth($this->getUser());
             return $this->response()->responseEntity(array(
                 "alumni" => $alumni->pbi($user->getPrivacy(), $sender),
--- web/components/User/Public.vue
+++ web/components/User/Public.vue
@@ -4,124 +4,122 @@
         <div v-if="visible">
             <span class="md-title">{{info.username}} 的个人页面</span>
             <br/>
-            <md-list class="md-douple-line" style="margin: 10px">
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">ID</span>
-                        <span>{{info.id}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">用户名</span>
-                        <span>{{info.username}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">中文名</span>
-                        <span>{{alumni.chineseName}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">英文名</span>
-                        <span>{{alumni.englishName}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">性别</span>
-                        <span>{{alumni.gender}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">生日</span>
-                        <span>{{alumni.birthday}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">初中信息</span>
-                        <span>{{alumni.juniorSchool}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">高中信息</span>
-                        <span>{{alumni.seniorSchool}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">常住国家或地区</span>
-                        <span>{{alumni.country}}</span>
-                    </div>
-                </md-list-item>
-                <md-divider></md-divider>
-                <div v-if="alumni.university">
+            <div v-if="alumni == null">
+                <span class="md-caption">该用户尚未通过实名认证。</span>
+            </div>
+            <div v-else>
+                <md-list class="md-douple-line">
                     <md-list-item>
                         <div class="md-list-item-text">
-                            <span class="md-caption">大学</span>
-                            <span>{{alumni.university}}</span>
+                            <span class="md-caption">ID</span>
+                            <span>{{info.id}}</span>
                         </div>
-                        <md-tooltip md-direction="bottom">{{ alumni.university }}</md-tooltip>
                     </md-list-item>
                     <md-divider></md-divider>
                     <md-list-item>
                         <div class="md-list-item-text">
-                            <span class="md-caption">专业</span>
-                            <span>{{alumni.major}}</span>
+                            <span class="md-caption">用户名</span>
+                            <span>{{info.username}}</span>
                         </div>
-                        <md-tooltip md-direction="bottom">{{ alumni.major }}</md-tooltip>
                     </md-list-item>
                     <md-divider></md-divider>
                     <md-list-item>
                         <div class="md-list-item-text">
-                            <span class="md-caption">常住地</span>
-                            <span>{{alumni.location}}</span>
+                            <span class="md-caption">中文名</span>
+                            <span>{{alumni.chineseName}}</span>
                         </div>
-                        <md-tooltip md-direction="bottom">{{ alumni.location }}</md-tooltip>
                     </md-list-item>
                     <md-divider></md-divider>
                     <md-list-item>
                         <div class="md-list-item-text">
-                            <span class="md-caption">联系方式</span>
-                            <span>{{alumni.onlineContact}}</span>
+                            <span class="md-caption">英文名</span>
+                            <span>{{alumni.englishName}}</span>
                         </div>
-                        <md-tooltip md-direction="bottom">{{ alumni.onlineContact }}</md-tooltip>
                     </md-list-item>
                     <md-divider></md-divider>
-                </div>
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span class="md-caption">个人简介</span>
-                        <span>{{alumni.personalInfo}}</span>
+                    <md-list-item>
+                        <div class="md-list-item-text">
+                            <span class="md-caption">性别</span>
+                            <span>{{alumni.gender}}</span>
+                        </div>
+                    </md-list-item>
+                    <md-divider></md-divider>
+                    <md-list-item>
+                        <div class="md-list-item-text">
+                            <span class="md-caption">生日</span>
+                            <span>{{alumni.birthday}}</span>
+                        </div>
+                    </md-list-item>
+                    <md-divider></md-divider>
+                    <md-list-item>
+                        <div class="md-list-item-text">
+                            <span class="md-caption">初中信息</span>
+                            <span>{{alumni.juniorSchool}}</span>
+                        </div>
+                    </md-list-item>
+                    <md-divider></md-divider>
+                    <md-list-item>
+                        <div class="md-list-item-text">
+                            <span class="md-caption">高中信息</span>
+                            <span>{{alumni.seniorSchool}}</span>
+                        </div>
+                    </md-list-item>
+                    <md-divider></md-divider>
+                    <md-list-item>
+                        <div class="md-list-item-text">
+                            <span class="md-caption">常住国家或地区</span>
+                            <span>{{alumni.country}}</span>
+                        </div>
+                    </md-list-item>
+                    <md-divider></md-divider>
+                    <div v-if="alumni.university">
+                        <md-list-item>
+                            <div class="md-list-item-text">
+                                <span class="md-caption">常住地</span>
+                                <span>{{alumni.location}}</span>
+                            </div>
+                            <md-tooltip md-direction="bottom">{{ alumni.location }}</md-tooltip>
+                        </md-list-item>
+                        <md-divider></md-divider>
+                        <md-list-item>
+                            <div class="md-list-item-text">
+                                <span class="md-caption">联系方式</span>
+                                <span>{{alumni.onlineContact}}</span>
+                            </div>
+                            <md-tooltip md-direction="bottom">{{ alumni.onlineContact }}</md-tooltip>
+                        </md-list-item>
+                        <md-divider></md-divider>
                     </div>
-                    <md-tooltip md-direction="bottom">{{ alumni.personalInfo }}</md-tooltip>
-                </md-list-item>
-                <md-divider></md-divider>
 
-            </md-list>
-            <md-speed-dial class="md-bottom-right" md-direction="top">
-                <md-speed-dial-target class="md-primary" @click="visit">
-                    <md-icon>send</md-icon>
-                </md-speed-dial-target>
-            </md-speed-dial>
+                </md-list>
+                <div v-if="alumni.university">
+                    <md-field>
+                        <label>大学</label>
+                        <md-textarea v-model="alumni.university" disabled></md-textarea>
+                    </md-field>
+                    <md-field>
+                        <label>专业</label>
+                        <md-textarea v-model="alumni.major" disabled></md-textarea>
+                    </md-field>
+                    <md-field>
+                        <label>工作</label>
+                        <md-textarea v-model="alumni.workInfo" disabled></md-textarea>
+                    </md-field>
+                </div>
+                <md-field>
+                    <label>个人简介</label>
+                    <md-textarea v-model="alumni.personalInfo" disabled></md-textarea>
+                </md-field>
+                <md-speed-dial class="md-bottom-right" md-direction="top">
+                    <md-speed-dial-target class="md-primary" @click="visit">
+                        <md-icon>send</md-icon>
+                    </md-speed-dial-target>
+                </md-speed-dial>
+            </div>
         </div>
         <div v-else>
             <span class="md-caption">该用户启用了反爬虫保护。</span>
         </div>
-
     </div>
 
 </template>
@@ -151,14 +149,16 @@
                     if(response.data["code"] === 200){
                         this.visible = true
                         this.alumni = response.data["data"]["alumni"]
-                        this.alumni.juniorSchool = this.getJuniorSchool(this.alumni.juniorSchool, this.alumni.juniorRegistration, this.alumni.juniorClass)
-                        this.alumni.seniorSchool = this.getSeniorSchool(this.alumni.seniorSchool, this.alumni.seniorRegistration, this.alumni.seniorClass)
-                        this.alumni.gender = this.getGender(this.alumni.gender)
-                        this.alumni.country = this.countries.filter((object)=>{
-                            return object.code === this.alumni.country
-                        })[0].name
-                        this.alumni.birthday = this.$moment(this.alumni.birthday).format("LL")
                         this.info = response.data["data"]["info"]
+                        if(this.alumni){
+                            this.alumni.juniorSchool = this.getJuniorSchool(this.alumni.juniorSchool, this.alumni.juniorRegistration, this.alumni.juniorClass)
+                            this.alumni.seniorSchool = this.getSeniorSchool(this.alumni.seniorSchool, this.alumni.seniorRegistration, this.alumni.seniorClass)
+                            this.alumni.gender = this.getGender(this.alumni.gender)
+                            this.alumni.country = this.countries.filter((object)=>{
+                                return object.code === this.alumni.country
+                            })[0].name
+                            this.alumni.birthday = this.$moment(this.alumni.birthday).format("LL")
+                        }
                     } else {
                         this.visible = false
                     }
