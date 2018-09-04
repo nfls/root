0bd9e513e48a76dc464b607a549372f34ee68462
Qingyang Hu <huqingyang2000@gmail.com>
2018-09-04 21:49:56 +0800

    Feature: add timeout to prevent overflow

--- bin/notification/server.py
+++ bin/notification/server.py
@@ -20,11 +20,12 @@ app.conf.beat_schedule = {
 }
 
 callback = config.apns_callback
-	
+
 
 @app.task(name='tasks.checkNotice')
 def checkNotice():
-    requests.get(config.task_url)
+    requests.get(config.task_url, timeout=3)
+
 
 @app.task(name='tasks.sendEmail')
 def sendEmail(sender, sender_name, receiver, subject, content, content_type):
@@ -32,14 +33,16 @@ def sendEmail(sender, sender_name, receiver, subject, content, content_type):
     sg.client.mail.send.post(request_body=mail.get())
     print("Email: Receiver: " + receiver + ", Subject: " + subject)
 
+
 @app.task(name='tasks.sendSMS')
 def sendSMS(receiver, template_code, params):
     cli = AliyunSMS(access_key_id=config.aliyun_id, access_secret=config.aliyun_key)
     cli.request(phone_numbers=receiver,
-                       sign='南外人',
-                       template_code=template_code,
-                       template_param=params)
+                sign='南外人',
+                template_code=template_code,
+                template_param=params)
     print("SMS: Receiver: " + receiver + ", Template: " + template_code + ", Params: " + json.dumps(params))
+
 
 @app.task(name='tasks.sendAPN')
 def sendAPN(token, callbackToken, title, subtitle, body, badge, custom):
@@ -54,7 +57,8 @@ def sendAPN(token, callbackToken, title, subtitle, body, badge, custom):
             status = 1
         if result.reason == "BadDeviceToken" or result.reason == "Unregistered":
             status = -1
-        print("APNS: Token: " + token + ", Title: " + title + ", Result: " + str(result.status_code) + "(" + (result.reason or "") + ")")
+        print("APNS: Token: " + token + ", Title: " + title + ", Result: " + str(result.status_code) + "(" + (
+                    result.reason or "") + ")")
     except:
         status = 0
 
