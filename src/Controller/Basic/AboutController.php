c4b7d8abfaa1c034df789b385a723413d7a3fb49
Qingyang Hu <hqy@nfls.io>
2018-11-03 19:54:37 +0800

    Revert "Paolu"
    This reverts commit 3ad4a337516b73702a4abc8dedddbf89b824b272.

--- src/Controller/Basic/AboutController.php
+++ src/Controller/Basic/AboutController.php
@@ -9,7 +9,6 @@ use App\Service\APNSService;
 use App\Service\CeleryService;
 use App\Service\MailService;
 use App\Service\SMSService;
-use GuzzleHttp\Client;
 use Longman\TelegramBot\Telegram;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,14 +17,5 @@ use Symfony\Component\Translation\TranslatorInterface;
 
 class AboutController extends AbstractController
 {
-    /**
-     * @Route("/tool/webp2png", methods="GET")
-     */
-    public function webp2png(Request $request) {
-        $webp = $request->query->get("image");
-        $client = new Client();
-        //$response = $client->do($webp);
-        //$response->getBody()->read()
-    }
 
 }
